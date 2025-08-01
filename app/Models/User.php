0b4e76c0bb2fe695cb50e0280b3691c0c7d4b3ec
Ega <eganugrahaid@gmail.com>
2025-08-01 08:29:55 +0700

    test role

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,6 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->hasOne(Role::class, 'id', 'role_id');
+        return $this->hasOne(Role::class, 'role_id');
     }
 }
